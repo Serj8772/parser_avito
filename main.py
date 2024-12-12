3ad31c3d9c1ccf969df7a1ba752661bc543da85b
Serj8772 <icq66623702@ya.ru>
2024-12-12 17:07:43 +0300

    INIT

---
+++ main.py
@@ -0,0 +1,82 @@
+# import zipfile # включить при использовании proxy
+# import re # включить при использовании proxy
+
+import fake_useragent
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import fake_useragent
+import telebot
+import csv
+
+from settings import token, id_channel, id_channel_for_logs, link, delay
+# from settings import PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS
+
+# настройка бота
+bot = telebot.TeleBot(token)
+
+# подменяем user-agent
+user_agent = fake_useragent.UserAgent().random
+
+opts = webdriver.ChromeOptions()
+opts.add_argument('user-agent=' + user_agent)
+
+opts.add_argument('--headless') # запуск в фоновом режиме
+opts.add_argument("--disable-blink-features=AutomationControlled")
+opts.add_argument('--no-sandbox') # убирает ошибку запуска в headless режиме на сервере
+
+
+prefs = {"profile.managed_default_content_settings.javascript": 2} # отключаем javascript
+opts.add_experimental_option("prefs", prefs) # отключаем javascript
+
+driver = webdriver.Chrome(options=opts)
+
+
+while True:
+
+    # загружаем ссылки из csv
+    filename = 'links.csv'
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        existing_rows = list(reader)
+
+    try:
+        driver.implicitly_wait(1)          # ожидание появление элемента в секундах
+
+        #  ссылка на страницу поиска
+        driver.get(link) # заходим на сайт
+        time.sleep(5)
+        driver.save_screenshot(f'screenshots/screenshot'+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'.png')
+        bot.send_message(id_channel_for_logs, text=f'⚡️⚡️⚡️ СТАРТ АВИТО ⚡️⚡️⚡️\n{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
+
+        # ищем объявления
+        ssilki = driver.find_elements(By.CLASS_NAME, value='iva-item-title-CdRXl')
+
+        for ssilka in ssilki:
+            link = ssilka.find_element(By.TAG_NAME, 'a')
+
+
+            # проверяем есть ли строка в файле, если нет то записываем ссылку
+            def write_to_csv(filename, data):
+
+                if [data] not in existing_rows:
+                    with open(filename, mode='a', newline='', encoding='utf-8') as file:
+                        writer = csv.writer(file)
+                        writer.writerow([data])
+                    print(data, '--', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                    bot.send_message(id_channel, text=data) # отправляем через бота в канал
+                    time.sleep(5)
+
+                else:
+                    print('строка уже существует', '--', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+
+            data = link.get_attribute('href')
+            write_to_csv(filename, data)
+
+
+    except Exception as ex:
+        print(ex)
+        bot.send_message(id_channel_for_logs, text=f'⚡️⚡️⚡️ Ошибка ⚡️⚡️⚡️\n{ex}')
+
+    time.sleep(delay * 60)
---
+++ settings.py
@@ -0,0 +1,20 @@
+delay = 60 # перерыв между сеансами в минутах
+
+# бот для отправки ссылок
+token = ''    # телеграм бот
+id_channel = ''   # телеграм канал
+# канал для логов.
+id_channel_for_logs = ''   # телеграм канал
+
+# ссылка на поиск
+link='https://www.avito.ru/lobnya/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?context=H4sIAAAAAAAA_wEjANz_YToxOntzOjg6ImZyb21QYWdlIjtzOjc6ImNhdGFsb2ciO312FITcIwAAAA&f=ASgBAgICA0SSA8gQ8AeQUrCzFP6hjwM&s=104'
+
+# проверка на анонимность
+link='https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html' # проверка на анонимность
+
+
+# # прокси
+# PROXY_HOST = '185.111.24.31'
+# PROXY_PORT = '8000'
+# PROXY_USER = 'fdyrQf'
+# PROXY_PASS = 'G7eMxA'
